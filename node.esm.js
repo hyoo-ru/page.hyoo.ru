4a16f88b5d408e249c3fea1090606b1904cfaf81
nin-jin <nin-jin@users.noreply.github.com>
2022-11-24 07:10:06 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -12436,7 +12436,8 @@ var $;
             if (key === 'id')
                 continue;
             if (typeof props[key] === 'string') {
-                ;
+                if (key in node)
+                    node[key] = props[key];
                 node.setAttribute(key, props[key]);
             }
             else if (props[key] &&
--- web.esm.js
+++ web.esm.js
@@ -2889,7 +2889,8 @@ var $;
             if (key === 'id')
                 continue;
             if (typeof props[key] === 'string') {
-                ;
+                if (key in node)
+                    node[key] = props[key];
                 node.setAttribute(key, props[key]);
             }
             else if (props[key] &&
