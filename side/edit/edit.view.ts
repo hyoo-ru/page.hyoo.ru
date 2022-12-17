2dea3c8989a936766ec677ae98a2517ee4d62932
jin <jin@hyoo.ru>
2022-12-17 15:17:22 +0800

    Fixed type

--- side/edit/edit.view.ts
+++ side/edit/edit.view.ts
@@ -2,6 +2,10 @@ namespace $.$$ {
 	
 	export class $hyoo_page_side_edit extends $.$hyoo_page_side_edit {
 		
+		publish() {
+			this.side().publish()
+		}
+		
 		download_md_name() {
 			return super.download_md_name().replace( '{filename}', this.title() )
 		}
