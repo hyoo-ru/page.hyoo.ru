34edf9989c2a75f6072120a20d8a138885b196c7
nin-jin <nin-jin@users.noreply.github.com>
2022-12-18 07:08:06 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.test.js
+++ web.test.js
@@ -4979,23 +4979,6 @@ var $;
 var $;
 (function ($) {
     $mol_test({
-        'triplets'() {
-            $mol_assert_equal(new $mol_time_interval('2015-01-01/P1M').end.toString(), '2015-02-01');
-            $mol_assert_equal(new $mol_time_interval('P1M/2015-02-01').start.toString(), '2015-01-01');
-            $mol_assert_equal(new $mol_time_interval('2015-01-01/2015-02-01').duration.toString(), 'PT2678400S');
-        },
-        'comparison'() {
-            const iso = '2021-01-02/2022-03-04';
-            $mol_assert_like(new $mol_time_interval(iso), new $mol_time_interval(iso));
-        },
-    });
-})($ || ($ = {}));
-//mol/time/interval/interval.test.ts
-;
-"use strict";
-var $;
-(function ($) {
-    $mol_test({
         'return result without errors'() {
             $mol_assert_equal($mol_try(() => false), false);
         },
