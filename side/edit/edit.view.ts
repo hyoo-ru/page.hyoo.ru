0b75c3749c7bbff14d7e7405165e81216eadd5ab
jin <jin@hyoo.ru>
2023-02-16 17:35:14 +0300

    + Copy as HTML

--- side/edit/edit.view.ts
+++ side/edit/edit.view.ts
@@ -6,15 +6,25 @@ namespace $.$$ {
 			this.side().publish()
 		}
 		
-		download_md_name() {
-			return super.download_md_name().replace( '{filename}', this.title() )
+		download_name() {
+			return super.download_name().replace( '{filename}', this.title() )
 		}
 
-		download_md_blob() {
+		download_blob() {
 			return new $mol_dom_context.Blob(
 				[ this.details() ],
 				{ type: 'text/markdown' },
 			)
+		}
+		
+		copy_html() {
+			return this.$.$hyoo_marked_to_html( `= ${ this.title() }\n${ this.details() }` )
+		}
+		
+		copy_md() {
+			return this.details()
+				.replaceAll( /^=+ /gm, ( prefix: string )=> prefix.replaceAll( '=', '#' ) ) // heading
+				.replaceAll( /^(" )+/gm, ( prefix: string )=> prefix.replaceAll( '" ', '> ' ) ) // quotes
 		}
 		
 	}
