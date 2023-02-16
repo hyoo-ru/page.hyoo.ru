175310cd199e6138d9df83dae784bdb186018926
jin <jin@hyoo.ru>
2023-02-16 18:18:17 +0300

    + permalink in export

--- side/edit/edit.view.ts
+++ side/edit/edit.view.ts
@@ -6,19 +6,28 @@ namespace $.$$ {
 			this.side().publish()
 		}
 		
+		@ $mol_mem
+		permalink() {
+			return this.$.$mol_state_arg.make_link({ '': this.side().id() })
+		}
+		
+		export_sign() {
+			return super.export_sign().replace( '{link}', this.permalink() )
+		}
+
 		download_name() {
 			return super.download_name().replace( '{filename}', this.title() )
 		}
 
 		download_blob() {
 			return new $mol_dom_context.Blob(
-				[ this.details() ],
+				[ `${ this.permalink() }\n\n${ this.details() }` ],
 				{ type: 'text/markdown' },
 			)
 		}
 		
 		copy_html() {
-			return this.$.$hyoo_marked_to_html( `= ${ this.title() }\n${ this.details() }` )
+			return this.$.$hyoo_marked_to_html( `= ${ this.title() }\n\n${ this.details() }\n\n${ this.export_sign() }` )
 		}
 		
 		copy_md() {
