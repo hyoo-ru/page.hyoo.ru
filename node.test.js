e23fa53a7ca12c49269f46b3342d7f74382096f5
nin-jin <nin-jin@users.noreply.github.com>
2023-02-17 14:43:09 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -9859,7 +9859,7 @@ var $;
         boxSizing: 'border-box',
         position: 'relative',
         minWidth: rem(2.5),
-        gap: $mol_gap.block,
+        gap: $mol_gap.space,
         border: {
             radius: $mol_gap.round,
         },
