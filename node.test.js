cd911f21ae0cac2bdcc764ccf9d559d5846dcfa3
nin-jin <nin-jin@users.noreply.github.com>
2023-03-04 13:34:10 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -2776,7 +2776,7 @@ var $;
 //mol/view/view/-css/view.css.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "7ca4736";
+let $hyoo_sync_revision = "a0ca8ec";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
@@ -4333,7 +4333,7 @@ var $;
     $.$hyoo_sync_masters = [
         `wss://sync.hyoo.ru/`,
         `wss://crowd.up.railway.app/`,
-        'wss://sync-hyoo-ru.herokuapp.com/',
+        `wss://crowd2.up.railway.app/`,
     ];
 })($ || ($ = {}));
 //hyoo/sync/masters/masters.ts
