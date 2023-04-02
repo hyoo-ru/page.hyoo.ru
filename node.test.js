63a22bcea13a2812c2b44d67a2c52bb0c3aa05a3
nin-jin <nin-jin@users.noreply.github.com>
2023-04-02 07:08:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -3337,7 +3337,7 @@ var $;
 //mol/book2/-css/book2.view.css.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "37cf8f4";
+let $hyoo_sync_revision = "1a9c6e5";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
