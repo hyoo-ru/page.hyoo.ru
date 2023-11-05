f0ca8cb945b93fcc891feca3cdd64180abf5dd04
nin-jin <nin-jin@users.noreply.github.com>
2023-11-05 07:07:58 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.mjs
+++ web.mjs
@@ -19005,6 +19005,7 @@ var $;
 var $;
 (function ($) {
     function $mol_base64_decode_web(base64Str) {
+        base64Str = base64Str.replace(/-/g, '+').replace(/_/g, '/');
         return new Uint8Array($mol_dom_context.atob(base64Str).split('').map(c => c.charCodeAt(0)));
     }
     $.$mol_base64_decode_web = $mol_base64_decode_web;
