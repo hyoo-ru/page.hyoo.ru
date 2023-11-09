bcedbe30201ebc84097e78c9d7801bbcbbb3fd01
jin <jin@hyoo.ru>
2023-11-09 12:24:04 +0300

    $hyoo_page_side_menu: Prevent infinity expand

--- side/menu/menu.view.ts
+++ side/menu/menu.view.ts
@@ -4,6 +4,7 @@ namespace $.$$ {
 		@ $mol_mem_key
 		item_expanded( id: $mol_int62_string, next?: boolean ): boolean {
 			const cur = this.side_current()
+			if( id === cur.id() ) return false
 			const path = [ ... cur.books() ]
 			if( cur.pages().length ) path.unshift( cur )
 			return next ?? ( $mol_mem_cached( ()=> this.item_expanded( id ) ) || path.some( book => book.id() === id ) )
