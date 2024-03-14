fcdbd06146f592e274495c7e8e907d605c05db85
jin <jin@hyoo.ru>
2024-03-14 10:54:52 +0300

    non blocking auto expand

--- side/menu/menu.view.ts
+++ side/menu/menu.view.ts
@@ -3,11 +3,13 @@ namespace $.$$ {
 		
 		@ $mol_mem_key
 		item_expanded( id: $mol_int62_string, next?: boolean ): boolean {
-			const cur = this.side_current()
-			const path = [ ... cur.books() ]
-			if( cur.pages().length ) path.unshift( cur )
-			if( id === path.at(-1)?.id() ) return false
-			return next ?? ( $mol_mem_cached( ()=> this.item_expanded( id ) ) || path.some( book => book.id() === id ) )
+			return $mol_wire_stale( ()=> {
+				const cur = this.side_current()
+				const path = [ ... cur.books() ]
+				if( cur.pages().length ) path.unshift( cur )
+				if( id === path.at(-1)?.id() ) return false
+				return next ?? ( $mol_mem_cached( ()=> this.item_expanded( id ) ) || path.some( book => book.id() === id ) )
+			} ) ?? $mol_mem_cached( ()=> this.item_expanded( id ) ) ?? false
 		}
 		
 		@ $mol_action
