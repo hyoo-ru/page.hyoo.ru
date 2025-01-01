89d9154c7e77686a742bf6fd92d6454a6d9de1ed
nin-jin <nin-jin@users.noreply.github.com>
2025-01-01 07:18:31 +0000

    Deploying to gh-pages from @ hyoo-ru/page.hyoo.ru@b23f1bb10fc8ae9e1e46acdb9ab2049e1e6dfd43 🚀

--- web.js
+++ web.js
@@ -6809,7 +6809,6 @@ var $;
             maxHeight: per(100),
             boxSizing: 'border-box',
             color: $mol_theme.text,
-            backdropFilter: blur(`3px`),
             ':focus': {
                 outline: 'none',
             },
@@ -19741,6 +19740,14 @@ var $;
                 const fetch_data = () => fetch(request).then(response => {
                     if (response.status !== 200)
                         return response;
+                    const headers = new Headers(response.headers);
+                    headers.set("Cross-Origin-Embedder-Policy", "credentialless");
+                    headers.set("Cross-Origin-Opener-Policy", "same-origin");
+                    response = new Response(response.body, {
+                        status: response.status,
+                        statusText: response.statusText,
+                        headers,
+                    });
                     event.waitUntil(caches.open('$mol_offline').then(cache => cache.put(request, response)));
                     return response.clone();
                 });
