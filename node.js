d806fb68fb89dce7120dd0de66599a5b8ae5c310
nin-jin <nin-jin@users.noreply.github.com>
2025-02-12 07:18:02 +0000

    Deploying to gh-pages from @ hyoo-ru/page.hyoo.ru@a38b8d8a46534f4b7921d2848b1cbdc493f713a4 🚀

--- node.js
+++ node.js
@@ -9886,7 +9886,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                const el = next.target;
+                const el = this.dom_node();
                 const from = el.selectionStart;
                 const to = el.selectionEnd;
                 try {
@@ -18822,6 +18822,10 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    function restack(error) {
+        error = new Error(error instanceof Error ? error.message : String(error), { cause: error });
+        $mol_fail_hidden(error);
+    }
     class $mol_crypto_sacred extends $mol_buffer {
         static size = 16;
         static make() {
@@ -18842,7 +18846,7 @@ var $;
             return sacred;
         }
         static async from_native(native) {
-            const buf = await $mol_crypto_native.subtle.exportKey('raw', native);
+            const buf = await $mol_crypto_native.subtle.exportKey('raw', native).catch(restack);
             const sacred = this.from(new Uint8Array(buf));
             sacred._native = native;
             return sacred;
@@ -18860,7 +18864,7 @@ var $;
             return this._native ?? (this._native = await $mol_crypto_native.subtle.importKey('raw', this, {
                 name: 'AES-CBC',
                 length: 128,
-            }, true, ['encrypt', 'decrypt']));
+            }, true, ['encrypt', 'decrypt']).catch(restack));
         }
         async encrypt(open, salt) {
             return new Uint8Array(await $mol_crypto_native.subtle.encrypt({
@@ -18868,7 +18872,7 @@ var $;
                 length: 128,
                 tagLength: 32,
                 iv: salt,
-            }, await this.native(), open));
+            }, await this.native(), open).catch(restack));
         }
         async decrypt(closed, salt) {
             return new Uint8Array(await $mol_crypto_native.subtle.decrypt({
@@ -18876,11 +18880,16 @@ var $;
                 length: 128,
                 tagLength: 32,
                 iv: salt,
-            }, await this.native(), closed));
+            }, await this.native(), closed).catch(restack));
         }
         async close(sacred, salt) {
-            const buf = new Uint8Array(this.buffer, this.byteOffset + 1, this.byteLength - 1);
-            return sacred.encrypt(buf, salt);
+            const buf = new Uint8Array(sacred.buffer, sacred.byteOffset + 1, sacred.byteLength - 1);
+            return this.encrypt(buf, salt);
+        }
+        async open(buf, salt) {
+            const buf2 = new Uint8Array(16);
+            buf2.set(await this.decrypt(buf, salt), 1);
+            return new $mol_crypto_sacred(buf2.buffer);
         }
     }
     __decorate([
