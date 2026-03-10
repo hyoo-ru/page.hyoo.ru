67738117476e5a3ace828247c797704028aa4340
nin-jin <nin-jin@users.noreply.github.com>
2026-03-10 07:42:37 +0000

    Deploying to gh-pages from @ hyoo-ru/page.hyoo.ru@8889910fc2ad6680e155146759678b256cce96e8 🚀

--- web.d.ts
+++ web.d.ts
@@ -7869,7 +7869,11 @@ declare namespace $.$$ {
 }
 
 declare namespace $ {
-    function $mol_crypto_hash(input: ArrayBufferView): Uint8Array<ArrayBuffer>;
+    function $mol_crypto2_hash(input: ArrayBufferView): Uint8Array<ArrayBuffer>;
+}
+
+declare namespace $ {
+    let $mol_crypto_hash: typeof $mol_crypto2_hash;
 }
 
 declare namespace $ {
@@ -7902,8 +7906,11 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_crypto_salt(): Uint8Array<ArrayBuffer>;
-    const $mol_crypto_salt_once: Uint8Array<ArrayBuffer>;
+    function $mol_crypto2_nonce(): Uint8Array<ArrayBuffer>;
+}
+
+declare namespace $ {
+    let $mol_crypto_salt: typeof $mol_crypto2_nonce;
 }
 
 declare namespace $ {
