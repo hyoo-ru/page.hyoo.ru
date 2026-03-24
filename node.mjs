84137b420b0fc0f0f42190c5570fd874c03ffc5b
nin-jin <nin-jin@users.noreply.github.com>
2026-03-24 07:54:48 +0000

    Deploying to gh-pages from @ hyoo-ru/page.hyoo.ru@8889910fc2ad6680e155146759678b256cce96e8 🚀

--- node.mjs
+++ node.mjs
@@ -6040,6 +6040,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
--- node.test.js
+++ node.test.js
@@ -6031,6 +6031,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
